5d0f292ae7cb88c0fb9f8b03a47bb4fe3bc485fc
HENRYMARTIN5 <HENRYMARTIN5@users.noreply.github.com>
2021-01-05 16:19:53 -0600

    messed up, deleting everything
    i messed up real bad

--- main.py
+++
@@ -1,4 +0,0 @@
-import compiler
-filetype="standard/.mth"
-runner = compiler.MythCompiler(filetype)
-runner.Run("tests.mth")
--- utils.py
+++
@@ -1,8 +0,0 @@
-from os import system, name
-def clear(): 
-    # for windows 
-    if name == 'nt': 
-        _ = system('cls') 
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = system('clear')
